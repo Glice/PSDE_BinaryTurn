1b0ece5481310f0fe51d35571c8eacbfc81e6ffd
Glice <glicinha.eu@gmail.com>
2017-11-30 10:23:18 -0200

    Commit inicial - Cenas, cartas virando, digitos em binário
    Projeto pronto para "Build and Run" em Android com cenas adicionadas,
    navegação entre telas, interação com as cartas (virar) e dígitos em
    binário sendo atualizados junto com as cartas.

---
+++ SimpleMobileGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+	public int resultado;
+	public Text myText;
+	public String myString;
+
+	// Use this for initialization
+	void Start () {
+		
+
+		//myString = GetComponent<CardController> ().front.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GetComponent<Text> ().text = resultado.ToString();
+		//gameObject.GetComponent<Text> ().text = myString;
+	}
+
+	public void AlterarValorDigito(int digito) {
+		
+		Debug.Log (digito);
+		//myText.text = GetComponent<CardController> ().front.ToString ();
+		//Debug.Log (myText.text);
+		myString = digito.ToString();
+//		myText = gameObject.GetComponent<Text>();
+//		myText.text = "2";
+	}
+	/*
+	public void AlterarResultado() {
+		Debug.Log ("alterou valor");
+
+		if (GetComponent<CardController> ().front == 1) {
+			gameObject.GetComponent<Text> ().text = "1";
+		} else {
+			gameObject.GetComponent<Text> ().text = "0";
+		}
+
+		//GetComponent<Text> ().text = resultado.ToString();
+		resultado += GetComponent<CardController> ().front * GetComponent<CardController> ().valor;
+		//gameObject.GetComponent<Text> ().text = resultado.ToString();
+
+	}*/
+}
